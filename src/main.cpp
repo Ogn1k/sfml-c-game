450fc69fc89f5a35b85f8eb5beac3c3a262d50be
Ogn1k <62839671+Ogn1k@users.noreply.github.com>
2020-06-15 23:46:33 +0500

    base commit

---
+++ src/main.cpp
@@ -0,0 +1,265 @@
+#include <SFML/Graphics.hpp>
+#include "parser/level.h"
+#include "view.h"
+#include <iostream>
+#include <sstream>
+//#include "mission.h"
+#include "iostream"
+//#include "map.h"
+#include <vector>
+#include <list>
+ 
+//#include "csv.h"
+ 
+using namespace sf;
+////////////////////////////////////Общий класс-родитель//////////////////////////
+class Entity {
+public:
+	std::vector<Object> obj;//вектор объектов карты
+	float dx, dy, x, y, speed,moveTimer;
+	int w,h,health;
+	bool life, isMove, onGround;
+	Texture texture;
+	Sprite sprite;
+	String name;
+	Entity(Image &image, String Name, float X, float Y, int W, int H)
+	{
+		x = X; y = Y; w = W; h = H; name = Name; moveTimer = 0;
+		speed = 0; health = 100; dx = 0; dy = 0;
+		life = true; onGround = false; isMove = false;
+		texture.loadFromImage(image);
+		sprite.setTexture(texture);
+		sprite.setOrigin(w / 2, h / 2);
+	}
+ 
+	FloatRect getRect()
+	{//ф-ция получения прямоугольника. его коорд,размеры (шир,высот).
+		return FloatRect(x, y, w, h);//эта ф-ция нужна для проверки столкновений 
+	}
+};
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////триггер/////////////////////////////
+class winTrigger :public Entity {
+public:
+	bool win;
+	winTrigger(Image& image, String Name, TileMap& lev, float X, float Y, int W, int H) :Entity(image, Name, X, Y, W, H)
+	{
+		win = false; obj = lev.getObjectsByName("winTrigger");
+	}
+
+	void triggerWin(float Dx, float Dy)
+	{
+		Text winT;
+		Texture wwinI;
+		Sprite winS;
+
+		for (int i = 0; i < obj.size(); i++)
+			if (getRect().intersects(obj[i].rect))
+			{
+				if (obj[i].name == "winTrigger")
+				{
+					if (Dy > 0 && Dy < 0 && Dx>0 && Dx < 0) {
+						winT.setString("Goal!");
+						winT.setPosition(520, 430);
+						std::cout << "aaa";
+					}
+				}
+			}
+	}
+	void update(float time)
+	{
+
+	}
+};
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////КЛАСС ИГРОКА////////////////////////
+class Player :public Entity {
+public:
+	enum { left, right, up, down, jump, stay } state;
+	int playerScore;
+	Player(Image& image, String Name, TileMap& lev, float X, float Y, int W, int H) :Entity(image, Name, X, Y, W, H) {
+		playerScore = 0; state = stay; obj = lev.getAllObjects();//инициализируем.получаем все объекты для взаимодействия персонажа с картой
+		if (name == "Player1") {
+			sprite.setTextureRect(IntRect(4, 19, w, h));
+		}
+	}
+
+	void control() 
+	{
+		if (Keyboard::isKeyPressed) {
+			if (Keyboard::isKeyPressed(Keyboard::A)) {
+				state = left; 
+				speed = 0.1;
+				if (Keyboard::isKeyPressed(Keyboard::LShift)) speed = 0.3;
+			}
+			if (Keyboard::isKeyPressed(Keyboard::D)) {
+				state = right;
+				speed = 0.1;
+				if (Keyboard::isKeyPressed(Keyboard::LShift)) speed = 0.3;
+
+			}
+
+			if ((Keyboard::isKeyPressed(Keyboard::Space)) && (onGround)) {
+				state = jump; dy = -0.6; onGround = false;
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::S)) {
+				state = down;
+			}
+			
+			
+		}
+
+		//std::cout << speed << "\n";
+	}
+ 
+
+ 
+	   void checkCollisionWithMap(float Dx, float Dy)
+	   {
+
+		   for (int i = 0; i<obj.size(); i++)//проходимся по объектам
+		   if (getRect().intersects(obj[i].rect))//проверяем пересечение игрока с объектом
+		   {
+			   if (obj[i].name == "solid")//если встретили препятствие
+			   {
+				   if (Dy>0)	{ y = obj[i].rect.top - h;  dy = 0; onGround = true; } //вверх 
+				   if (Dy<0)	{ y = obj[i].rect.top + obj[i].rect.height;   dy = 0; } //вниз
+				   if (Dx>0)	{ x = obj[i].rect.left - w; }// влево...
+				   if (Dx<0)	{ x = obj[i].rect.left + obj[i].rect.width; } //вправо...
+			   }
+			   if (obj[i].name == "box")//если встретили препятствие
+			   {
+				   if (Dy > 0) { y = obj[i].rect.top - h;  dy = 0; onGround = true; }
+				   //if (Dy < 0) { y = obj[i].rect.top + obj[i].rect.height;   dy = 0; }
+				   //if (Dx > 0) { x = obj[i].rect.left - w; }
+				   //if (Dx < 0) { x = obj[i].rect.left + obj[i].rect.width; }
+			   }
+		   }
+	   }
+	    
+	   void update(float time)
+	   {
+		   control();
+		   switch (state)
+		   {
+		   case right:dx = speed; break;
+		   case left:dx = -speed; break;
+		   case up: break;
+		   case down: dx = 0; break;
+		   case stay: break;
+		   }
+		   x += dx*time;
+		   checkCollisionWithMap(dx, 0);
+		   
+		   y += dy*time;
+		   checkCollisionWithMap(0, dy);
+		   
+		   sprite.setPosition(x + w / 2, y + h / 2);
+		   if (health <= 0){ life = false; }
+		   if (!isMove){ speed = 0; }
+		   setPlayerCoordinateForView(x, y);
+		   if (life) { setPlayerCoordinateForView(x, y); }
+		   dy = dy + 0.0015*time;
+	   } 
+};
+ ////////////////////////////////////////////////////////////////////////////////////
+ 
+ ///////////////////////////////////Класс врага//////////////////////////////////////
+class Enemy :public Entity{
+public:
+	Enemy(Image &image, String Name, TileMap& lev, float X, float Y, int W, int H) :Entity(image, Name, X, Y, W, H){
+		obj = lev.getObjectsByName("solid");//инициализируем.получаем нужные объекты для взаимодействия врага с картой
+		if (name == "EnemySpawn"){
+			sprite.setTextureRect(IntRect(0, 0, w, h));
+			dx = 0.1;
+		}
+	}
+ 
+	void checkCollisionWithMap(float Dx, float Dy)
+	{
+		
+		for (int i = 0; i<obj.size(); i++)//проходимся по объектам
+		if (getRect().intersects(obj[i].rect))//проверяем пересечение игрока с объектом
+		{
+			if (obj[i].name == "solid"){//если встретили препятствие (объект с именем solid)
+				if (Dy>0)	{ y = obj[i].rect.top - h;  dy = 0; onGround = true; }
+				if (Dy<0)	{ y = obj[i].rect.top + obj[i].rect.height;   dy = 0; }
+				if (Dx>0)	{ x = obj[i].rect.left - w;  dx = -0.1; sprite.scale(-1, 1); }
+				if (Dx<0)	{ x = obj[i].rect.left + obj[i].rect.width; dx = 0.1; sprite.scale(-1, 1); }
+			}
+		}
+	}
+ 
+	void update(float time)
+	{
+		if (name == "EnemySpawn"){
+			//moveTimer += time;if (moveTimer>3000){ dx *= -1; moveTimer = 0; }//меняет направление примерно каждые 3 сек
+			checkCollisionWithMap(dx, 0);
+			x += dx*time;
+			sprite.setPosition(x + w / 2, y + h / 2);
+			if (health <= 0){ life = false; }
+		}
+	}
+};
+//////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////Основная функция main//////////////////////////////////
+int main()
+{
+	RenderWindow window(VideoMode(1040, 860), "window");
+	view.reset(FloatRect(0, 0, 640, 480));
+	TileMap lvl;
+	//создали экземпляр класса уровень
+	lvl.load("D:/Users/Public/source/repos/test 2/x64/Release/map/NewTile5.tmx");//загрузили в него карту, внутри класса с помощью методов он ее обработает.
+
+	Image heroImage;
+	heroImage.loadFromFile("D:/Users/Public/source/repos/test 2/x64/Release/textures/MilesTailsPrower.gif");
+	Texture textUre;
+	textUre.loadFromImage(heroImage);
+ 
+	Image easyEnemyImage;
+	easyEnemyImage.loadFromFile("D:/Users/Public/source/repos/test 2/x64/Release/textures/link.png");
+	easyEnemyImage.createMaskFromColor(Color(0, 64, 64));
+
+	Image TriggerImage1;
+	TriggerImage1.loadFromFile("D:/Users/Public/source/repos/test 2/x64/Release/textures/win.png");
+ 
+	Object player=lvl.getObject("player");//объект игрока на нашей карте.задаем координаты игроку в начале при помощи него
+	Object easyEnemyObject = lvl.getObject("EnemySpawn");//объект легкого врага на нашей карте.задаем координаты игроку в начале при помощи него
+	Object TriggerObject = lvl.getObject("winTrigger"); //win trigger
+ 
+	Player p(heroImage, "Player1", lvl, player.rect.left, player.rect.top, 40, 30);//передаем координаты прямоугольника player из карты в координаты нашего игрока
+	Enemy easyEnemy(easyEnemyImage, "EnemySpawn", lvl, easyEnemyObject.rect.left, easyEnemyObject.rect.top, 60, 60);//передаем координаты прямоугольника easyEnemy из карты в координаты нашего врага
+	winTrigger Trigger(TriggerImage1, "TriggerWin", lvl, TriggerObject.rect.left, TriggerObject.rect.left, 32, 32);
+
+	Clock clock;
+	while (window.isOpen())
+	{
+ 
+		float time = clock.getElapsedTime().asMicroseconds();
+ 
+		clock.restart();
+		time = time / 800;
+		
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();	
+		}	
+
+		p.update(time);
+		easyEnemy.update(time);
+		window.setView(view);
+		window.clear(Color(77,83,140));
+
+		window.draw(lvl);//рисуем карту
+		window.draw(Trigger.sprite);
+		window.draw(easyEnemy.sprite);
+		window.draw(p.sprite);
+
+		window.display();
+	}
+	return 0;
+}
+//////////////////////////////////////////////////////////////////////////////////////////////
---
+++ src/parser/level.h
@@ -0,0 +1,270 @@
+﻿#include <SFML/Graphics.hpp>
+#include "tinyxml2.h"
+#include <iostream>
+#include <cctype>
+
+class Object
+{
+public:
+	Object(float x, float y, float width, float height) : rect(x, y, width, height)
+	{
+
+	}
+
+	int         GetPropertyInt(const std::string& name); //номер свойства объекта в нашем списке
+	float       GetPropertyFloat(const std::string& name);
+	std::string GetPropertyString(const std::string& name);
+
+	std::string name;
+	std::string type;
+	std::map<std::string, std::string> properties; //создаём ассоциатиный массив. ключ - строковый тип, значение - строковый
+	sf::FloatRect rect; //тип Rect с нецелыми значениями
+	//sf::Sprite sprite;
+};
+
+class TileMap : public sf::Drawable
+{
+public:
+	~TileMap();
+
+	bool load(const std::string& tmx_file_path); //возвращает false если не получилось загрузить
+
+	Object               getObject(const std::string& name);
+	std::vector<Object>  getObjectsByName(const std::string& name);
+	std::vector<Object>  getObjectsByType(const std::string& type);
+	std::vector<Object>& getAllObjects();
+	
+private:
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const; //рисуем в окно 
+
+	sf::Texture* texture;
+	std::vector<sf::VertexArray> tile_layers;
+	std::vector<Object>          objects;
+};
+
+TileMap::~TileMap()
+{
+	if (texture)
+		delete texture;
+}
+
+bool TileMap::load(const std::string& tmx_file_path){
+	tinyxml2::XMLDocument document;
+
+	if (document.LoadFile(tmx_file_path.c_str()) != tinyxml2::XML_SUCCESS)
+	{
+		std::cout << "Loading file " << tmx_file_path << " failed..." << std::endl;
+		return false;
+	} //возвращает false если не получилось загрузить (функция)
+
+	tinyxml2::XMLElement* root_element = document.FirstChildElement("map");
+	const size_t tile_width = 32; //std::stoul(root_element->Attribute("tilewidth"));
+	const size_t tile_height = 32; //std::stoul(root_element->Attribute("tileheight"));
+
+	auto tileset = root_element->FirstChildElement("tileset");
+	const size_t tile_count = 1024; //std::stoul(tileset->Attribute("tilecount"));
+	const size_t columns = 32; //std::stoul(tileset->Attribute("columns"));
+
+	//auto image = tileset->FirstChildElement("image");
+	std::string path = "D:/Users/Public/source/repos/test 2/x64/Release/textures/smb3.png"; //image->Attribute("source");
+
+	while (!isalpha(path.front())) 
+		path.erase(0, 1);          
+
+	texture = new sf::Texture();
+
+	if (!texture->loadFromFile(path))
+		return false;
+
+	std::vector<sf::Vector2f> texture_grid;
+	texture_grid.reserve(tile_count);
+
+	size_t rows = tile_count / columns;
+
+	for (size_t y = 0u; y < rows; ++y)
+		for (size_t x = 0u; x < columns; ++x)
+			texture_grid.emplace_back(sf::Vector2f((float)x * tile_width, (float)y * tile_height));
+ 
+	for (auto layer = root_element->FirstChildElement("layer");
+		layer != nullptr;
+		layer = layer->NextSiblingElement("layer"))
+	{
+		if (!layer)
+		{
+			std::cerr << "Bad map. No layer information found." << std::endl;
+			return false;
+		}//*/
+
+		const size_t width = std::stoul(layer->Attribute("width"));
+		const size_t height = std::stoul(layer->Attribute("height"));
+
+		tinyxml2::XMLElement* data = layer->FirstChildElement("data");
+		std::string dirty_string = data->GetText(), buffer;
+
+		std::vector<size_t> csv_array;
+		csv_array.reserve(dirty_string.size());
+
+		for (auto& character : dirty_string)
+		{
+			if (isdigit(character))
+				buffer += character;
+			else
+				if (!buffer.empty())
+				{
+					csv_array.push_back(std::stoi(buffer));
+					buffer.clear();
+				}
+		}
+		csv_array.shrink_to_fit();
+
+		sf::VertexArray vertices;
+		vertices.setPrimitiveType(sf::Quads);
+
+		for (size_t y = 0u, index = 0u; y < height; ++y)
+		{
+			for (size_t x = 0u; x < width; ++x, ++index)
+			{
+				size_t tile_num = csv_array[index];
+
+				if (tile_num)
+				{   
+					sf::Vertex leftTop;     
+					sf::Vertex rightTop;    
+					sf::Vertex rightBottom; 
+					sf::Vertex leftBottom;  
+
+					leftTop.position = sf::Vector2f((float)x * tile_width, (float)y * tile_height);
+					rightTop.position = sf::Vector2f(((float)x + 1) * tile_width, (float)y * tile_height);
+					rightBottom.position = sf::Vector2f(((float)x + 1) * tile_width, (float)(y + 1) * tile_height);
+					leftBottom.position = sf::Vector2f((float)x * tile_width, (float)(y + 1) * tile_height);
+
+					leftTop.texCoords = sf::Vector2f(texture_grid[tile_num - 1].x, texture_grid[tile_num - 1].y);
+					rightTop.texCoords = sf::Vector2f(texture_grid[tile_num - 1].x + tile_width, texture_grid[tile_num - 1].y);
+					rightBottom.texCoords = sf::Vector2f(texture_grid[tile_num - 1].x + tile_width, texture_grid[tile_num - 1].y + tile_height);
+					leftBottom.texCoords = sf::Vector2f(texture_grid[tile_num - 1].x, texture_grid[tile_num - 1].y + tile_height);
+
+					vertices.append(leftTop);
+					vertices.append(rightTop);
+					vertices.append(rightBottom);
+					vertices.append(leftBottom);
+				}
+			}
+		}
+		tile_layers.push_back(std::move(vertices));
+	}
+
+	for (auto object_group = root_element->FirstChildElement("objectgroup");
+		object_group != nullptr;
+		object_group = object_group->NextSiblingElement("objectgroup"))
+	{
+		for (auto object = object_group->FirstChildElement("object");
+			object != nullptr;
+			object = object->NextSiblingElement("object"))
+		{
+			std::string object_name;
+			if (object->Attribute("name"))
+				object_name = object->Attribute("name");
+
+			std::string object_type;
+			if (object->Attribute("type"))
+				object_type = object->Attribute("type");
+
+			float x = std::stof(object->Attribute("x"));
+			float y = std::stof(object->Attribute("y"));
+
+			float width{}, height{};
+
+			if (object->Attribute("width") && object->Attribute("height"))
+			{
+				width = std::stof(object->Attribute("width"));
+				height = std::stof(object->Attribute("height"));
+			}
+
+			Object new_object(x, y, width, height);
+			new_object.name = object_name;
+			new_object.type = object_type;
+
+			auto properties = object->FirstChildElement("properties");
+
+			if (properties)
+			{
+				for (auto property = properties->FirstChildElement("property");
+					property != nullptr;
+					property = property->NextSiblingElement("property"))
+				{
+					std::string name, value;
+
+					if (property->Attribute("name"))
+						name = property->Attribute("name");
+
+					if (property->Attribute("value"))
+						value = property->Attribute("value");
+
+					new_object.properties[name] = value;
+				}
+			}
+			objects.emplace_back(std::move(new_object));
+		}
+	}
+	return true;
+}
+
+Object TileMap::getObject(const std::string& name)
+{
+	auto found = std::find_if(objects.begin(), objects.end(), [&](const Object& obj)
+		{
+			return obj.name == name;
+		});
+
+	return *found;
+}
+
+std::vector<Object> TileMap::getObjectsByName(const std::string& name)
+{
+	std::vector<Object> objects_by_name;
+
+	std::copy_if(objects.begin(), objects.end(), std::back_inserter(objects_by_name), [&](const Object& obj)
+		{
+			return obj.name == name;
+		});
+
+	return objects_by_name;
+}
+
+std::vector<Object> TileMap::getObjectsByType(const std::string& type)
+{
+	std::vector<Object> objects_by_type;
+
+	std::copy_if(objects.begin(), objects.end(), std::back_inserter(objects_by_type), [&](const Object& obj)
+		{
+			return obj.type == type;
+		});
+
+	return objects_by_type;
+}
+
+std::vector<Object>& TileMap::getAllObjects()
+{
+	return objects;
+}
+
+void TileMap::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	for (auto& layer : tile_layers)
+		target.draw(layer, texture);
+}
+
+int Object::GetPropertyInt(const std::string& name)
+{
+	return std::stoi(properties[name]);
+}
+
+float Object::GetPropertyFloat(const std::string& name)
+{
+	return std::stof(properties[name]);
+}
+
+std::string Object::GetPropertyString(const std::string& name)
+{
+	return properties[name];
+}
